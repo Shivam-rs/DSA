80df8f667c10390ee5fa66b3e73a554494bc2a08
Shivam97.rs@gmail.com <crikfan5@gmail.com>
2024-02-23 20:21:17 -0500

    Int to column header

--- src/main/java/scaler/Combinatorics/ExcelColumnTitle.java
+++ src/main/java/scaler/Combinatorics/ExcelColumnTitle.java
@@ -3,7 +3,19 @@ package scaler.Combinatorics;
 public class ExcelColumnTitle {
 
     public static void main(String[] args) {
-        //hi
-        System.out.println();
+
+        System.out.println(convertToTitle(78));
+    }
+
+    //Like how we convert decimal to binary we have to convert this number to base 26
+    //Important point to note is to make 0 based indexing we subtract 1 before performing modulo
+    public static String convertToTitle(int A) {
+        String ans = "";
+        while(A > 0){
+            ans = (char)((A-1)%26 + 'A') + ans;
+
+            A = (A-1)/26;
+        }
+        return ans;
     }
 }
