5f87cb8fcac2f509b1585c8d02bae2c09e75bd18
Shivam97.rs@gmail.com <crikfan5@gmail.com>
2024-05-11 13:19:10 -0400

    Base for custom linked list

---
+++ src/main/java/scaler/linkedlist/ListNode.java
@@ -0,0 +1,41 @@
+package scaler.linkedlist;
+
+public class ListNode {
+
+  public int val;
+  public ListNode next;
+
+  public ListNode() {
+
+  }
+
+  ListNode(int x) {
+    val = x;
+    next = null;
+  }
+
+
+//  public void insert(int data) {
+//    if (head == null) {
+//      head = new ListNode(data);
+//
+//    } else {
+//      ListNode temp = head;
+//      while (temp.next != null) {
+//        temp = temp.next;
+//      }
+//      temp.next = new ListNode(data);
+//    }
+//    size++;
+//  }
+
+//  public void insertNode(int position, int val) {
+//      ListNode temp = new ListNode(val);
+//
+//      if(position == 0) {
+//
+//      }
+//  }
+
+
+}
